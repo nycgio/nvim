02a93bd8fc81025270e84c13632ecd1306773f1c
gio <gio@gio.com>
2025-09-17 10:46:22 -0400

    ignore certain files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,7 +44,26 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require('telescope').setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            ".git",
+            ".cache",
+            "%.o",
+            "%.a",
+            "%.out",
+            "%.class",
+            "%.pdf",
+            "%.mkv",
+            "%.mp4",
+            "%.zip"
+          },
+        },
+      })
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",

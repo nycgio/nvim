836c853bb02674680de3b986ad94e758d57cb3b3
gio <gio@gio.com>
2025-09-17 13:10:05 -0400

    lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -88,6 +88,31 @@ return {
       "williamboman/mason-lspconfig.nvim",
       "folke/neodev.nvim",
     },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "pyright",
+          "rust_analyzer",
+          "gopls",
+        },
+        automatic_installation = true,
+      })
+
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Auto setup all installed servers
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+      })
+    end,
   },
 
   -- Autocompletion
